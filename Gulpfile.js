1416ff75dbf73cae01fec112f7a9f70c00911e1b
Chris Samuel <ksamuel.chris@icloud.com>
2016-07-12 19:44:19 -0400

    added dependencies to gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var production = process.env.NODE_ENV === 'production';
+
+var dependencies = [
+  'alt',
+  'react',
+  'react-dom',
+  'react-router',
+  'underscore'
+];
