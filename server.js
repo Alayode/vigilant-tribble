69beaf73421949299821d7942936a2ed87e0c411
Chris Samuel <ksamuel.chris@icloud.com>
2016-07-21 16:33:03 -0400

    Create Routes for handling the random characters for the voting procedure

--- server.js
+++ server.js
@@ -11,6 +11,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+/**
+ * GET /api/characters
+ * Returns 2 random characters of the same gender that have not been voted yet.
+ */
+
+ app.get('/api/characters', function(req,res,next){
+    var randomGender = _.sample(choices);
+    var choices = [ Female , Male ];
+
+    //use the Character object find method to find a random gender who hasnt beeen
+    //voted on yet. Limit only 2.
+
+    Character.find({ random :{$near:[Math.random(),0]}})
+    .where('voted',false)
+    .where('gender',randomGender)
+    .limit(2)
+    .exec(function(err,characters){
+      if(err) return next (err);
+
+      //create the next conditional
+      if(characters.length === 2){
+        return res.send(characters);
+      }
+
+      // create and use the Character update method.
+      Character.update({})
+
+    })
+ })
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
