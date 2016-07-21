1558acd567f228cbe20d9f0853addbaa5658003f
Chris Samuel <ksamuel.chris@icloud.com>
2016-07-20 20:50:30 -0400

    completed HomeAction class for handling the winner and loser

--- app/actions/HomeActions.js
+++ app/actions/HomeActions.js
@@ -1,7 +1,39 @@
-//Chris Kayode Samuel
+/*Chris Kayode Samuel*/
 /*
 
 File: HomeActions.js
 Description: will handle the winner and loser Character of our voter app.
 
 */
+
+import alt from '.../alt';
+
+class HomeActions {
+  constructor(){
+    this.generateActions('getTwoCharactersSuccess','getTwoCharactersFail','voteFail');
+  }
+
+
+getTwoCharacters(){
+$.ajax({url: '/api/characters'})
+  .done(data => {
+    this.actions.getTwoCharactersFail(jqXhr.responseJSON.message);
+  });
+}
+vote(winner,loser){
+  $.ajax({
+    type:'PUT',
+    url:'/api/characters',
+    data:{winner:winner , loser:loser}
+  })
+  .done(() => {
+    this.actions.getTwoCharacters();
+  })
+    .fail(jqXhr) => {
+      this.actions.voteFail(jqXhr.responseJSON.message);
+    });
+  }
+}
+
+
+export default alt.createActions(HomeActions);
